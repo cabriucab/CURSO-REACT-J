6b779d8d787eee3e856ab548ec5b05e887991824
cabriucab <cabriu@hotmail.com>
2022-10-13 21:11:11 -0300

    Ultimo commit

---
+++ app/src/header.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Header(){
+
+return(
+<>
+
+
+<header className="encabezado">
+
+<h1 id="logo">LOGO</h1>
+<nav id="menu">
+
+<a  href="#">link</a>
+<a href="#">link</a>
+<a href="#">link</a>
+</nav>
+
+</header>
+
+
+
+</>
+
+
+)
+
+
+}
+
+export default Header
---
+++ app/src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Main(){
+
+return(<p>MI APP NUEVA</p>)
+
+}
+
+export default Main
